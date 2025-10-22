2892b8d87e4ddec2d60040a6e39f67963fc43b89
theadarshbalaji <adarshbhyd@gmail.com>
2025-10-22 10:51:49 +0530

    routing update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
-import Profile from "./pages/Profile";
-import Cart from "./pages/Cart";
 import Products from "./pages/Products";
+import Cart from "./pages/Cart";
+import Profile from "./pages/Profile";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import CustomerDashboard from "./pages/CustomerDashboard";
+import RetailerDashboard from "./pages/RetailerDashboard";
+import WholesalerDashboard from "./pages/WholesalerDashboard";
 
 function App() {
   return (
@@ -12,16 +17,26 @@ function App() {
         <Link to="/products" style={{ margin: "10px" }}>Products</Link>
         <Link to="/cart" style={{ margin: "10px" }}>Cart</Link>
         <Link to="/profile" style={{ margin: "10px" }}>Profile</Link>
+        <Link to="/signup" style={{ margin: "10px" }}>Signup</Link>
+        <Link to="/login" style={{ margin: "10px" }}>Login</Link>
       </nav>
 
       <Routes>
+        {/* Existing routes */}
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/profile" element={<Profile />} />
+
+        {/* New auth + dashboard routes */}
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/customer-dashboard" element={<CustomerDashboard />} />
+        <Route path="/retailer-dashboard" element={<RetailerDashboard />} />
+        <Route path="/wholesaler-dashboard" element={<WholesalerDashboard />} />
       </Routes>
     </Router>
   );
 }
 
-export default App;
+export default App;
